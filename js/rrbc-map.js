411bd9b23678595fd6b2063a67cf511e028dee90
Jacob DeBenedetto <jacob.debenedetto@investisdigital.com>
2022-08-07 13:46:35 -0700

    Add custom pins with information

--- js/rrbc-map.js
+++ js/rrbc-map.js
@@ -2,41 +2,52 @@
     'use strict';
     
     $(document).ready(function(){
-        var map = L.map('leaflet-map', { dragging: !L.Browser.mobile, tap: false }).setView([33.6502553, -111.9788628], 10);
+        var debug = true;
+        var map = L.map('leaflet-map', { dragging: !L.Browser.mobile, tap: false, zoomControl: false }).setView([33.6502553, -111.9788628], 10);
         map.scrollWheelZoom.disable();
-        L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', { maxZoom: 18 }).addTo(map);
+        L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
         
-        var latlngs = [
-            [33.6517575, -111.9774950],
-            [33.6515787, -111.9774896],
-            [33.6515709, -111.9784390],
-            [33.6513453, -111.9784337],
-            [33.6512828, -111.9785356],
-            [33.6500758, -111.9785302],
-            [33.6500668, -111.9788575],
-            [33.6501391, -111.9788575]
-        ];
-        var path = L.polyline.antPath(latlngs, {
-            "delay": 1000,
-            "dashArray": [
-                10,
-                50
-            ],
-            "weight": 5,
-            "color": "#0000FF",
-            "pulseColor": "#FFFFFF",
-            "paused": false,
-            "reverse": false,
-            "hardwareAccelerated": true
+        // Load paths from JSON file
+        $.getJSON('./json/paths.json?v=' + new Date().getTime(), function(json) {
+            var latlngs = json;
+            var path = L.polyline.antPath(latlngs, {
+                "delay": 1000,
+                "dashArray": [
+                    10,
+                    20
+                ],
+                "weight": 5,
+                "color": "#0000FF",
+                "pulseColor": "#FFFFFF",
+                "paused": false,
+                "reverse": false,
+                "hardwareAccelerated": true
+            });
+            map.addLayer(path);
+            map.fitBounds(path.getBounds(), { padding: [16, 16] });
         });
+
+        // Load pins from JSON file
+        var group = new L.featureGroup([]);
+        $.getJSON('./json/pins.json?v=' + new Date().getTime(), function(json) {
+            json.forEach(function(pin) {
+                var icon = L.icon({ iconUrl: pin.icon, iconSize: [32, 32], iconAnchor: [16, 32], popupAnchor: [0, -32] });
+                var marker = L.marker(pin.geo, { icon: icon }).addTo(group);
+                marker.bindPopup(
+                    '<div class="title">'+ pin.title + '</div>' +
+                    '<div class="description">'+ pin.description + '</div>'
+                );
+            })
+        });
+        group.addTo(map);
+        L.control.zoom({ position:'bottomright' }).addTo(map);
         
-        
-        
-        map.addLayer(path);
-        map.fitBounds(path.getBounds(), { padding: [16, 16] });
-        
+        // Mouse hover listener
         map.addEventListener('mousemove', function(ev) {
-            console.log('[' + ev.latlng.lat.toFixed(7) + ', ' + ev.latlng.lng.toFixed(7) + ']');
+            // Debug geo-coordinates
+            if (debug == true) {
+                console.log('[' + ev.latlng.lat.toFixed(7) + ', ' + ev.latlng.lng.toFixed(7) + ']');
+            }
         });
 
         // Update map resize when scrolling is triggered
